d913f6cf60ea36f0d1f81c75fa8176846683c272
service <service@polystar.com.ua>
2019-08-13 16:30:42 +0300

    refactor Message component

--- src/client/js/Chat.js
+++ src/client/js/Chat.js
@@ -8,24 +8,48 @@ export default class Chat extends Component {
     super(props)
 
     this.state = {
+      input: '',
+      message: {},
+      chatMessages: [],
       client: socket()
     }
 
     this.sendMessage = this.sendMessage.bind(this)
+    this.inputHandler = this.inputHandler.bind(this)
+  }
+  inputHandler (e) {
+    e.preventDefault()
+    this.setState({ input: e.target.value })
   }
   sendMessage (e) {
     e.preventDefault()
-    this.state.client.sendMessage('message sample')
+    this.state.client.sendMessage(this.state.input)
+    this.addMsgToChat()
+  }
+  addMsgToChat () {
+    this.setState(state => {
+      const chatMessages = [...state.chatMessages, {
+        timeStamp: new Date(),
+        from: 'client',
+        msg: state.input
+      }]
+      return {
+        chatMessages,
+        input: ''
+      }
+    })
   }
   render () {
+    let { chatMessages, input } = this.state
     return (
       <div className="chat-container">
         <div className="chat-messages">
-          <Message messageType="client" messageText="lorem ipsum sit dolor amet"/>
-          <Message operatorName="Charlie Goo" messageText="lorem ipsum sit dolor amet"/>
+          {chatMessages.length ? chatMessages.map((message, index) => (
+            <Message key={index} message={message}/>
+          )) : `Have no messages yet`}
         </div>
         <div className="chat-active-zone">
-          <textarea className="chat-input" type="text" placeholder="type your question here..."/>
+          <textarea className="chat-input" type="text" value={input} onChange={(e) => this.inputHandler(e)} placeholder="type your question here..."/>
           <button className="chat-send" onClick={(e) => this.sendMessage(e)}>Send</button>
         </div>
       </div>
--- src/client/js/Message.js
+++ src/client/js/Message.js
@@ -3,19 +3,17 @@ import PropTypes from 'prop-types'
 
 export default class Message extends Component {
   static propTypes = {
-    messageType: PropTypes.string,
-    operatorName: PropTypes.string,
-    messageText: PropTypes.string
+    message: PropTypes.object
   }
   constructor (props) {
     super(props)
   }
   render () {
-    let { messageType, operatorName, messageText } = this.props
+    let { message } = this.props
     return (
-      <div className={`chat-message-wrap ${messageType === 'client' ? 'chat-message-wrap--client' : 'chat-message-wrap--server'}`}>
-        <div className="chat-message__author"><span>{messageType === 'client' ? 'You:' : `${operatorName}:` }</span></div>
-        <div className="chat-message">{messageText}</div>
+      <div className={`chat-message-wrap ${message.from === 'client' ? 'chat-message-wrap--client' : 'chat-message-wrap--server'}`}>
+        <div className="chat-message__author"><span>{message.from === 'client' ? 'You:' : `${message.from}:` }</span></div>
+        <div className="chat-message">{message.msg}</div>
       </div>
     )
   }
